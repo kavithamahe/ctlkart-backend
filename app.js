8e1ad47f5e87336ec83d51e41c95ade064facb3b
kavitha <kavithamahe2@gmail.com>
2019-10-14 17:15:41 +0530

    kavi

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use('/api', api);
 
 // start server
 
-var port = process.env.NODE_ENV === 'development' ? 3000 : 3000;     
+var port = process.env.NODE_ENV === 'production' ? 5000 : 5000;     
 
 var server = app.listen(port, function () {
     console.log('Server listening on port ' + port); 
